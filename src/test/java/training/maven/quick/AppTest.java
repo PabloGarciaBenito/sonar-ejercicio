de0658e8896b61bf527c6c45137231d6ce73f6b7
PABLO GARCIA BENITO <6002859@viewnext.com>
2024-03-01 10:54:57 +0100

    Tests modificados

--- src/test/java/training/maven/quick/AppTest.java
+++ src/test/java/training/maven/quick/AppTest.java
@@ -7,22 +7,22 @@ public class AppTest {
     private final HolaMundo sut = new HolaMundo();
 
     @Test
-    public void addition_should_add_two_numbers() {
+    public void AdditionShouldAddTwoNumbers() {
         assertEquals(4, sut.addition(2, 2));
     }
 
     @Test
-    public void subtraction_should_subtract_second_number_from_first_number() {
+    public void SubtractionShouldSubtractSecondNumberFromFirstNumber() {
         assertEquals(6, sut.subtraction(8, 2));
     }
 
     @Test
-    public void multiplication_should_multiply_two_numbers() {
+    public void MultiplicationShouldMultiplyTwoNumbers() {
         assertEquals(32, sut.multiplication(8, 4));
     }
 
     @Test
-    public void division_should_divide_first_number_with_the_second() {
+    public void DivisionShouldDivideFirstNumberWithTheSecond() {
         assertEquals(5,sut.division(40, 8));
     }
 }
